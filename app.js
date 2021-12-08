1964004f59eb20dedcb86fc9e669fedfce08655b
Pete Seeling <peteseeling@gmail.com>
2021-12-08 15:22:41 -0500

    2ndraft

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 
-const formEl = document.getElementById('form');
+import { displayCurrentPoll } from "./utils.js";
+
+const form = document.getElementById('form');
 const optionATitleEl = document.getElementById('option-a-title');
 const optionAVotesEl = document.getElementById('option-a-votes');
 const optionBTitleEl =  document.getElementById('option-b-title');
 const optionBVotesEl = document.getElementById('option-b-votes');
+const pollQuestionEl = document.getElementById('poll-question');
 
 const optionAAddButton = document.getElementById('option-a-add');
 const optionASubtractButton = document.getElementById('option-a-subtract');
@@ -11,6 +14,8 @@ const optionBAddButton = document.getElementById('option-b-add');
 const optionBSubtractButton = document.getElementById('option-b-subtract');
 const closePollButton = document.getElementById('close-poll');
 const startPollButton = document.getElementById('start-poll');
+const pastPollsEl = document.getElementById('past-polls');
+
 
 let question = '';
 let optionAVotes = 0;
@@ -19,30 +24,52 @@ let optionATitle = '';
 let optionBTitle = '';
 
 const pastPollsArray = [];
-
+//add votes to option A
 optionAAddButton.addEventListener('click', () => {
   optionAVotes++;
 
   optionAVotesEl.textContent = optionAVotes;
 })
-
+//add votes to option B
 optionBAddButton.addEventListener('click', () => {
   optionBVotes++;
 
   optionBVotesEl.textContent = optionBVotes;
 })
-// let state
+//Subtract votes to option A
 optionASubtractButton.addEventListener('click', () => {
   optionAVotes--;
 
   optionAVotesEl.textContent = optionAVotes;
 })
-
+//Subtract votes to option B
 optionBSubtractButton.addEventListener('click', () => {
   optionBVotes--;
 
   optionBVotesEl.textContent = optionBVotes;
 })
+
+form.addEventListener('submit', (e) =>{
+  e.preventDefault();
+
+  const data = new FormData(form);
+  question = data.get('question');
+  optionATitle = data.get("option-a");
+  optionBTitle = data.get("option-b");
+  
+  pollQuestionEl.textContent = question;
+  optionATitleEl.textContent = optionATitle;
+  optionBTitleEl.textContent = optionBTitle;
+})
+
+closePollButton.addEventListener('click', () => {
+  form.reset();
+})
+
+
+
+
+
 // set event listeners 
   // get user input
   // use user input to update state 
---
+++ utils.js
@@ -0,0 +1,7 @@
+
+
+export function displayCurrentPoll(){
+    pollQuestionEl.textContent = question;
+    optionATitleEl.textContent = optionATitle;
+    optionBTitleEl.textContent = optionBTitle;
+    }
