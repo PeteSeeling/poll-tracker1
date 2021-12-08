d4f441fa79478f99ba27275c2178e530dd1fc6dd
Pete Seeling <peteseeling@gmail.com>
2021-12-08 14:25:26 -0500

    1st commit. Html finished

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
-// import functions and grab DOM elements
+
+const formEl = document.getElementById('form');
+const optionATitleEl = document.getElementById('option-a-title');
+const optionAVotesEl = document.getElementById('option-a-votes');
+const optionBTitleEl =  document.getElementById('option-b-title');
+const optionBVotesEl = document.getElementById('option-b-votes');
+
+const optionAAddButton = document.getElementById('option-a-button');
+const optionASubtractButton = document.getElementById('option-a-subtract');
+const optionBAddButton = document.getElementById('option-b-button');
+const optionBSubtractButton = document.getElementById('option-b-subtract');
+const closePollButton = document.getElementById('close-poll');
+const startPollButton = document.getElementById('start-poll');
+
+let question = '';
+let optionAVotes = 0;
+let optionBVotes = 0;
+let optionATitle = '';
+let optionBTitle = '';
+
+const pastPollsArray = [];
+
+optionAAddButton.addEventListener('click', () => {
+  optionAVotes++;
+  
+  optionAVotesEl.textContent = optionAVotes;
+})
+
 
 // let state
 
